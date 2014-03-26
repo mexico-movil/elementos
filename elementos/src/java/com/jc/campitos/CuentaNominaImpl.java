423139da11e1ba997a53a6c2734c6e57bf50cbf8
campitos <campitos@campitoslap2>
2014-03-25 21:37:13 -0600

    Primera actualizacion

---
+++ elementos/src/java/com/jc/campitos/CuentaNominaImpl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jc.campitos;
+
+/**
+ *
+ * @author campitos
+ */
+class CuentaNominaImpl extends Cuenta implements ServicioCuenta {
+
+
+    public String crearCuenta() {
+return " Cuenta de nomina creada";
+    }
+    
+}
---
+++ elementos/web/WEB-INF/jsp/hola.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : hola
+    Created on : Jan 31, 2014, 6:52:56 PM
+    Author     : campitos
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Esta es una pagina JSP</title>
+    </head>
+    <body>
+        <h1>Hola Mundo</h1>
+    </body>
+</html>
