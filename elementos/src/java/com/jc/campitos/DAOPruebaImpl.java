9f4e2db742ba5cd4987a44477d8ae361c0f35be6
campitos <campitos@campitoslap2>
2014-04-04 20:12:44 -0600

    Actualizacion Hibernate

--- elementos/src/java/com/jc/campitos/DAOPruebaImpl.java
+++ elementos/src/java/com/jc/campitos/DAOPruebaImpl.java
@@ -1,7 +1,9 @@
 
 package com.jc.campitos;
 
+import java.util.ArrayList;
 import org.hibernate.Query;
+
 
 public class DAOPruebaImpl extends DAO {
        
@@ -13,6 +15,25 @@ public class DAOPruebaImpl extends DAO {
        commit();
        close();
        System.out.println("Registro insertado");
+    }
+    
+    //segunda operacion actualizar
+    public void actualizarRegistro( Prueba p){
+        begin();
+        getSession().update(p);
+        commit();
+        close();
+        System.out.println("registro actualizado");
+    }
+    
+    //tercera operacion: buscarTodos
+    public ArrayList<Prueba> buscarTodos(){
+        begin();
+   Query q=     getSession().createQuery("from Prueba");
+       ArrayList<Prueba> registros=(ArrayList<Prueba>) q.list();
+       commit();
+       close();
+        return registros;
     }
     
     
--- elementos/src/java/com/jc/campitos/ProbarOperaciones.java
+++ elementos/src/java/com/jc/campitos/ProbarOperaciones.java
@@ -3,12 +3,17 @@ package com.jc.campitos;
 
 public class ProbarOperaciones {
   public static void main(String args[]){
+      
          DAOPruebaImpl d=new DAOPruebaImpl();
          Prueba p=new Prueba();
+         //p.setId(2);
+        // p.setNombre("Mikey Mouse");
+        // d.actualizarRegistro(p); 
+         for(Prueba pr:d.buscarTodos() ){
+            System.out.println( pr.getNombre()+ " tu id : "+pr.getId());
+         }
             
-           //  p=d.buscarPorId(3);
-             System.out.println(p.getNombre());
-            // d.insertarRegistro(p) ;
+         
   }  
     
 }
